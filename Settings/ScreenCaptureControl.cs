24409e1f09adbde1b7370aa3c2b91ff9c736734b
gerhard.NOSPAM@gmail.com <gerhard.NOSPAM@gmail.com@69de40ad-164b-fc4a-e498-0b667ebbd87e>
2012-05-11 21:21:23 +0000

    * Increase height when importing screenshots for 580P

--- Settings/ScreenCaptureControl.cs
+++ Settings/ScreenCaptureControl.cs
@@ -33,7 +33,6 @@ namespace ZoneFiveSoftware.SportTracks.Device.Globalsat
         {
             InitializeComponent();
 
-
             buttonCaptureScreen.CenterImage = CommonResources.Images.Refresh16;
             buttonSave.CenterImage = CommonResources.Images.Save16;
 
@@ -44,49 +43,31 @@ namespace ZoneFiveSoftware.SportTracks.Device.Globalsat
             {
                 ThemeChanged(Plugin.Instance.Application.VisualTheme);
             }
-
         }
-
-
-
-
-
-
 
         #region Public methods
 
         public void ShowPage()
         {
-
         }
-
-
 
         public void ThemeChanged(ITheme visualTheme)
         {
-
             groupBoxScreenCapture.ForeColor = visualTheme.ControlText;
             this.BackColor = visualTheme.Control;
-
         }
 
         public void UICultureChanged(CultureInfo culture)
         {
-            //buttonImportDeviceConfig.Text = Properties.Resources.UI_Settings_ImportConfigButton_Text;
-            //buttonExportDeviceConfig.Text = Properties.Resources.UI_Settings_ExportConfigButton_Text;
-
             toolTip1.SetToolTip(buttonCaptureScreen, Properties.Resources.UI_Settings_ScreenCapture_CaptureScreen);
             toolTip1.SetToolTip(buttonSave, Properties.Resources.UI_Settings_ScreenCapture_SaveImage);
-            
 
             groupBoxScreenCapture.Text = Properties.Resources.UI_Settings_ScreenCapture_Title;
-
         }
         #endregion
 
         private void buttonCaptureScreen_Click(object sender, EventArgs e)
         {
-
             try
             {
                 IJobMonitor jobMonitor = new JobMonitor();
@@ -94,11 +75,11 @@ namespace ZoneFiveSoftware.SportTracks.Device.Globalsat
                 GlobalsatProtocol device2 = device.Device(jobMonitor);
                 if (device2 != null)
                 {
-
                     Bitmap screenshot = device2.GetScreenshot(jobMonitor);
 
                     if (screenshot != null)
                     {
+                        this.groupBoxScreenCapture.Height += screenshot.Height - pictureBox1.Height;
                         pictureBox1.ClientSize = new Size(screenshot.Width, screenshot.Height);
                         pictureBox1.Image = screenshot;
 
@@ -111,15 +92,11 @@ namespace ZoneFiveSoftware.SportTracks.Device.Globalsat
                 MessageDialog.Show(ex.Message, Properties.Resources.UI_Settings_ScreenCapture_Error, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
         }
-
 
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
-
-
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 try
@@ -135,7 +112,6 @@ namespace ZoneFiveSoftware.SportTracks.Device.Globalsat
                     return;
                 }
             }
-
         }
     }
 }
